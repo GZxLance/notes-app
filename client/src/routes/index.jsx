94fa217590524f1a94cba3a42defe6c1d76927d4
GZxLance <1367649451@qq.com>
2025-03-24 21:22:01 +0800

    分类功能实现

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import Register from '@/pages/Register';
 import Login from '@/pages/Login';
 import Home from '@/pages/Home';
+import Categories from '@/pages/Categories';
+import CategoryNotes from '@/pages/CategoryNotes';
 
 const AppRoutes = () => {
   return (
@@ -10,6 +12,8 @@ const AppRoutes = () => {
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
       <Route path="/" element={<Home />} />
+      <Route path="/categories" element={<Categories />} />
+      <Route path="/notes/categories/:categoryId" element={<CategoryNotes />} />
     </Routes>
   );
 };
