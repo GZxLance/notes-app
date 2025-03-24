b980be4655a0039e4a77e6034ec19bd2438259a8
GZxLance <1367649451@qq.com>
2025-03-24 21:40:32 +0800

    笔记功能实现

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -5,6 +5,8 @@ import Login from '@/pages/Login';
 import Home from '@/pages/Home';
 import Categories from '@/pages/Categories';
 import CategoryNotes from '@/pages/CategoryNotes';
+import Notes from '@/pages/Notes';
+import Note from '../pages/Note';
 
 const AppRoutes = () => {
   return (
@@ -14,6 +16,8 @@ const AppRoutes = () => {
       <Route path="/" element={<Home />} />
       <Route path="/categories" element={<Categories />} />
       <Route path="/notes/categories/:categoryId" element={<CategoryNotes />} />
+      <Route path="/notes" element={<Notes />} />
+      <Route path="/notes/:id" element={<Note />} />
     </Routes>
   );
 };
