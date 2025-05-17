26d44386fc189cda520bae18c570fa06640094ec
GZxLance <1367649451@qq.com>
2025-05-17 17:32:40 +0800

    远程版本1.0

--- server/app.js
+++ server/app.js
@@ -11,8 +11,8 @@ dotenv.config();
 const app = express();
 
 // 允许特定源，并设置 Access-Control-Allow-Credentials
-const allowedOrigins = ["http://localhost:5173", "http://localhost:3000"];
-// const allowedOrigins = ["http://124.221.65.20:8080"];
+// const allowedOrigins = ["http://localhost:5173", "http://localhost:3000"];
+const allowedOrigins = ["http://124.221.65.20:8080"];
 app.use(
   cors({
     origin: function (origin, callback) {
